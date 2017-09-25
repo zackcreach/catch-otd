f811e572d5d53de910f3072a55688364d3ee02df
zackcreach <zackcreach@gmail.com>
2017-09-25 17:15:49 -0400

    Initial commit.

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className='catch-of-the-day'>
+        <div className='menu'>
+          <Header />
+        </div>
+        <Order />
+        <Inventory />
+      </div>
+    )
+  }
+}
---
+++ catch-of-the-day/src/index.js
@@ -0,0 +1,11 @@
+// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+import './css/style.css';
+
+import StorePicker from './components/StorePicker';
+
+render(
+  <StorePicker />,
+  document.getElementById('main')
+);
